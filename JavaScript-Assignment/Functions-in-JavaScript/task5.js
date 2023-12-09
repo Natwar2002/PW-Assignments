e4f1aa0c19705664e59a194f08314993d1d82d6d
[Natwar Patidar] <[natvarp6062@gmail.com]>
2023-12-09 09:58:21 +0530

    Add 2 new task

--- JavaScript-Assignment/Functions-in-JavaScript/task5.js
+++ JavaScript-Assignment/Functions-in-JavaScript/task5.js
@@ -1,5 +1,7 @@
 function factorial(number) {
-    if (number == 1 || number == 0) {
+    if (number < 0 ) {
+        console.log ("Please enter a positive integer")
+    } else if (number == 1 || number == 0) {
         return 1
     } else {
         return number * factorial(number-1)
@@ -8,6 +10,8 @@ function factorial(number) {
 
 let number1 = 5
 let number2 = 8
+let number3 = -2
 
 console.log(`Factorial of the number : ${number1} is : ${factorial(number1)}`);
 console.log(`Factorial of the number : ${number2} is : ${factorial(number2)}`);
+console.log(`Factorial of the number : ${number3} is : ${factorial(number3)}`);
